fc68f2867fd1f54db75fe200b0889cd56909b42a
Blade Vipex <vipex@vipexsoft.com>
2015-04-01 16:56:46 +0200

    Create flipMyPage.user.js

---
+++ flipMyPage.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        flipMyPage 
+// @namespace   http://www.vipexsoft.com
+// @description Flips horizontally the page
+// @include     http://*
+// @include     https://*
+// @version     0.1
+// @grant       none
+// ==/UserScript==
+
+var css = document.createElement('style');
+css.type = 'text/css';
+
+var styles = 'body { \
+-moz-transform: scale(-1, 1); \
+-webkit-transform: scale(-1, 1); \
+-o-transform: scale(-1, 1); \
+-ms-transform: scale(-1, 1); \
+transform: scale(-1, 1); \
+}';
+
+if (css.styleSheet) css.styleSheet.cssText = styles;
+else css.appendChild(document.createTextNode(styles));
+
+document.getElementsByTagName("head")[0].appendChild(css);
